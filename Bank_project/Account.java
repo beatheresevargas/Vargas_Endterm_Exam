bd099dcbdb7ae7fbf28baa2aafa6b32cff8f00ea
Bea Vargas <btpvargas@gmail.com>
2021-05-18 15:55:14 +0800

    Third Commit

--- Bank_project/Account.java
+++ Bank_project/Account.java
@@ -2,4 +2,28 @@ package Bank_project;
 
 public abstract class Account {
 
+	int accountNum;
+	double bal;
+	
+	public Account(int num)
+	{
+		accountNum=num;
+		setBal(0.0);
+	}
+	
+	public void setBalance(double balance)
+	{
+		bal = balance;
+	}
+	
+	public abstract int getAccountNum()
+	{
+		return num;
+	}
+	
+	public abstract double getBalance()
+	{
+		return bal;
+	}
+	
 }
--- Bank_project/Checking.java
+++ Bank_project/Checking.java
@@ -1,5 +1,25 @@
 package Bank_project;
 
-public class Checking {
+public class Checking extends Account {
+	
+	public Checking (int accountNum)
+	{
+		super(accountNum);
+	}
+	
+	public int getAccountNum()
+	{
+		return accountNum;
+	}
+	
+	public double getBalance()
+	{
+		return bal;
+	}
+	
+	public String getInfo()
+	{
+		return("Checking Account Information \n Acct No: " + getAccountNum() + "Balance:" + getBalance());
+	}
 
 }
